f8170808fd7422d2174531ac2d1591c4307df3da
sunqi1984 <59982543+sunqi1984@users.noreply.github.com>
2020-02-06 18:54:54 -0500

    Update daterange.py

--- daterange.py
+++ daterange.py
@@ -1,5 +1,5 @@
-import sys,time,os,json,datetime
-
+import sys,time,os,json,datetime,operator
+dateCount = {}
 for root,dirs, files in os.walk('data'):
     #print(root,dirs,files)
     path = root.split(os.sep)
@@ -9,4 +9,17 @@ for root,dirs, files in os.walk('data'):
         f = open(fp, 'r')
         for line in f:
             data = json.loads(line)
-            #print(len(data))
+            payload = data['payload']
+            payload = json.loads(payload)
+#            print(payload['received'])
+            k = payload['received'].split('T')[0]
+            #print (k)
+            if k in dateCount.keys():
+                dateCount[k]+=1
+            else:
+                dateCount[k] = 1
+
+# 0 == key , 1 == value
+sorted_dc = sorted(dateCount.items(),key=operator.itemgetter(1),reverse=True)
+
+print(sorted_dc)
